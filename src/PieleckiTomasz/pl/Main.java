b27e3d1ff94f34882b0324bdf9aa0ad61faa7dc4
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2021-09-23 12:58:51 +0200

    Try

--- src/PieleckiTomasz/pl/Main.java
+++ src/PieleckiTomasz/pl/Main.java
@@ -1,4 +1,5 @@
 package PieleckiTomasz.pl;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,8 +7,41 @@ public class Main {
             int[] myNumbers = {1, 2, 3};
             System.out.println(myNumbers[10]);
         } catch (Exception e) {
-            System.out.println("Something went wrong.");
+            System.out.println("Something went wrong. array is to [3]");
+        } finally {
+            System.out.println("Finish try and catch");
+        }
+// Enter age, later 18 years indicate no access to disco above give access
+        System.out.println("Tell me what ur is age?");
+
+        int age;age = getInt();
+        try {
+            if (age >= 18) {
+                System.out.println("u have Access to Disco party");
+            } else {
+                System.out.println("U don't have access on Disco Party");
+            }
+        } catch (Exception e) {
+            System.out.println("Age not given");
+        }finally {
+            System.out.println("End of maturity test");
         }
     }
+    public static int getInt(){
+        return new Scanner(System.in).nextInt();
+    }
 }
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
